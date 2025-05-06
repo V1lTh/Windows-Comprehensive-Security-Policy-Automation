87551a6c2368a19e6ef48547a39948486d7e2c49
César <120366469+V1lTh@users.noreply.github.com>
2025-05-06 13:04:58 +0200

    Update runASadmin.bat

--- runASadmin.bat
+++ runASadmin.bat
@@ -1,5 +1,5 @@
 @echo off
 set "scriptPath=%~dp0\callfiles.ps1"
 cd /d %~dp0
-powershell.exe -ExecutionPolicy RemoteSigned -File "%scriptPath%"
+powershell.exe -ExecutionPolicy Bypass -File "%scriptPath%"
 sleep 1
